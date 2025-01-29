a094a0dfb4b756346bea1596083a9804c13e6ab4
Rubens Alves <alvesrubens926@gmail.com>
2025-01-29 11:24:55 -0300

    Desafio numero aleatorio

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Aleatorio from "./componentes/Aleatorio";
 import Primeiro from "./componentes/Primeiro";
 import ComParametro from "./componentes/ComParametro";
 import Fragmento from "./componentes/Fragmento";
@@ -8,6 +9,7 @@ export default function App(props) {
   return (
     <div id="app">
       <h1>Fundamento React </h1>
+      <Aleatorio min={1} max={60} />
       <Fragmento />
       <ComParametro titulo="Situação do aluno " aluno="Pedro" nota={9.9} />
       <Primeiro></Primeiro>
